6642e95bc5887e6ad185849f3bc36be70f26e578
udaykiranchilumula <udaykiranchilumula80@gmail.com>
2025-06-10 00:00:11 +0530

    port changed to aws ip

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://100.26.33.212:3000',
+        target: 'http://localhost:3000',
         secure: false,
       },
     },
