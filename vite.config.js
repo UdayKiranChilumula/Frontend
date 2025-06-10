30731e7d5d78b408ebf9bb6ef2603daea96500e1
udaykiranchilumula <udaykiranchilumula80@gmail.com>
2025-06-11 02:49:08 +0530

    xjn

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: `http://"${process.env.API_HOST}":3000`,
-        secure: false,
-      },
-    },
-  },
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: `http://"${process.env.API_HOST}":3000`,
+//         secure: false,
+//       },
+//     },
+//   },
 
-  plugins: [react()],
-});
+//   plugins: [react()],
+// });
